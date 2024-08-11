529e89b945e661eeca26a0a737824afab7378242
Muthu kumar <mk2778555@gmail.com>
2024-08-12 00:49:56 +0530

    Function methods

---
+++ code.js
@@ -0,0 +1,66 @@
+// let arr=[1,2,3,4,5]
+// let k=3;
+// function rotate(arr,k){
+//     for (let i=0;i<arr.length;i++){
+//         arr.push(arr[i])
+// }
+// for(let i=0;i<k;i++){
+//     arr.shift()
+// } 
+// return arr
+// }
+// console(rotate(arr,k));
+// let arr=[1,2,3,4,5]
+// let max=0;
+// for (let i=0;i<arr.length;i++){
+//     if(max<arr[i]){
+//         max=arr[i]
+//     }   
+// }
+// console.log(max);
+// let arr=[]
+// for (let i=0;i<arr.length;i++){
+//     if(min>arr[i]){
+//         min=arr[i]
+//     }
+// }
+// console.log(min);
+// let arr=[1,2,3,4,5,6,7]
+// max=0;
+// for (let i=;i>arr.length;i++){
+//     console.log(arr[i]);
+// }
+// function add(hi){
+//  return{ 
+//     post:hi+hello,
+//  leger:hi-hello,
+//  posting:hi*hello}  
+// }
+
+// function mult(m){
+// return{
+
+// }
+// }
+// let a1=423;
+// let a2=345;
+// add(a1,a2)
+// mult(a1,a2)
+// console.log(mult(1,add(423,345)));
+
+// console.log(432,342);
+
+
+
+function awe(hi,hello){
+return hi*hello;
+}
+
+function awk(fun,how,are){
+return how+are+fun
+}
+
+let total =awk(awe(213,231),234,234)
+
+console.log(total);
+
